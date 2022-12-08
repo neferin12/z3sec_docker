652d96e43eab4fe710b2219542aa657a9e8aebf2
Julian Pollinger <julian@pollinger.dev>
2022-12-08 17:38:57 +0100

    fix ci

--- getBash.sh
+++ getBash.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+# Pull the Docker image
+docker pull neferin12/z3sec
+
+# Create a Docker container from the image
+docker create --name z3sec neferin12/z3sec
+
+# Open a bash shell in the container
+docker exec -it z3sec bash
